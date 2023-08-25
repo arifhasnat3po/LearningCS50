202dd8f7a23e95d3b674ad83c494e7b2341bf0ed
Arif <ahparif.16@gmail.com>
2023-08-26 03:42:50 +0600

    Third Commit

---
+++ Lec -1 C Programming/stringslocation.c
@@ -0,0 +1,15 @@
+#include<cs50.c>
+#include<stdio.h>
+
+int main(void){
+
+      
+      string s = "Hi!";
+      string t = "Bye!";
+
+      printf("%s\n", s);
+      printf("%s\n", t);
+      printf("%i %i %i %i\n", s[0], s[1], s[2]);
+
+
+}
