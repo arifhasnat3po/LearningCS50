81aac0fc5794d4ce8a008581ea8684f44fd09de9
Arif <ahparif.16@gmail.com>
2023-09-02 02:30:21 +0600

    SQL

--- Lec - 7 SQl/favorite.py
+++ Lec - 7 SQl/favorite.py
@@ -1,12 +1,14 @@
 import csv
-languages = [] # list of languages
+languages = {}
 with open ("favorites.csv", "r") as file :
     reader = csv.DictReader(file)
     
     for row in reader:
         language = row["language"].strip().upper()
-        if not row["language"] in languages:
-            languages.append(row["language"])
+        if not language in languages:
+            languages[language] = 0
+        
+        languages[language] += 1
     
-for language in languages:
-    print(language)
+for language in sorted(languages):
+    print(language, languages[language])
