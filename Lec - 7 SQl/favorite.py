8980792a3d83b22d8fd90e5b1997220e6d0c1e1f
Arif <ahparif.16@gmail.com>
2023-09-02 02:08:34 +0600

    SQL

--- Lec - 7 SQl/favorite.py
+++ Lec - 7 SQl/favorite.py
@@ -4,9 +4,9 @@ with open ("favorites.csv", "r") as file :
     reader = csv.DictReader(file)
     
     for row in reader:
-        language = row["language"].strip().uppercase()
+        language = row["language"].strip().upper()
         if not row["language"] in languages:
-            languages.append(language)
+            languages.append(row["language"])
     
 for language in languages:
     print(language)
